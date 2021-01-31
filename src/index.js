ae8279cf76b67a769bb6e33608ea2860bcc0eddf
Josh Frank <eurosewusa@gmail.com>
2021-01-31 14:13:27 -0500

    aioli

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const taskForm = document.getElementById("create-task-form");
+const taskInputField = document.getElementById( "new-task-description" );
+const taskList = document.getElementById( "tasks" );
+
+document.addEventListener( "DOMContentLoaded", () => {
+  taskForm.addEventListener( "submit", addTaskToList );
+} );
+
+const addTaskToList = function ( submitButton ) {
+  submitButton.preventDefault();
+  taskList.innerHTML += `<li>${ taskInputField.value }</li>`;
+  taskInputField.value = "";
+}
